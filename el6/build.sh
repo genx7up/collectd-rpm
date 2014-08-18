0d782c3fc681f36d3796aaf7ab05de99f895feae
genx7up <genx7up@gmail.com>
2014-08-17 18:06:50 -0700

    Minor changes, runs in a docker container.
    /data is shared volume

--- el6/build.sh
+++ el6/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Install tools required to build the rpms
-yum install -y rpm-build yum-utils rpmdevtools
+yum install -y rpm-build yum-utils rpmdevtools gcc
 
 # Install EPEL
 rpm -ivh http://mir01.syntis.net/epel/6/i386/epel-release-6-8.noarch.rpm
@@ -13,11 +13,8 @@ wget -c http://collectd.org/files/collectd-5.4.1.tar.bz2
 # Extract source
 tar xvf collectd-5.4.1.tar.bz2
 
-# Create build directory
-mkdir -p /root/rpmbuild/SOURCES/
-
 # Copy sources in build directory
-cp collectd-5.4.1.tar.bz2 /root/rpmbuild/SOURCES/
+cp collectd-5.4.1.tar.bz2 /rpmbuild/SOURCES/
 
 # Fix the specfile to match collected version
 sed -i s/5.4.0/5.4.1/ collectd-5.4.1/contrib/redhat/collectd.spec
@@ -29,5 +26,5 @@ yum-builddep -y collectd-5.4.1/contrib/redhat/collectd.spec
 rpmbuild -bb collectd-5.4.1/contrib/redhat/collectd.spec
 
 # Copy RPMs back to the host
-cp -rv /root/rpmbuild/RPMS/ /vagrant
+cp -rv /rpmbuild/RPMS/ /data
 
