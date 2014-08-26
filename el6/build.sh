5270be52caa4062fe73388801e8232b0f28b147f
genx7up <genx7up@gmail.com>
2014-08-25 18:34:57 -0700

    Update build.sh

--- el6/build.sh
+++ el6/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+# Install EPEL if needed
+rpm -ivh http://mir01.syntis.net/epel/6/i386/epel-release-6-8.noarch.rpm
+
 # Install tools required to build the rpms
 yum install -y rpm-build yum-utils rpmdevtools gcc
-
-# Install EPEL
-rpm -ivh http://mir01.syntis.net/epel/6/i386/epel-release-6-8.noarch.rpm
 
 # Go to /tmp to get and extract collectd source base
 cd /tmp
@@ -14,6 +14,7 @@ wget -c http://collectd.org/files/collectd-5.4.1.tar.bz2
 tar xvf collectd-5.4.1.tar.bz2
 
 # Copy sources in build directory
+mkdir -p /rpmbuild/SOURCES/
 cp collectd-5.4.1.tar.bz2 /rpmbuild/SOURCES/
 
 # Fix the specfile to match collected version
