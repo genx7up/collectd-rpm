bad648163babdfbf7f2d076b8ff1ceb7f0cbde0e
genx7up <genx7up@users.noreply.github.com>
2015-04-28 21:35:30 -0700

    Bump to 5.4.2

--- el6/build.sh
+++ el6/build.sh
@@ -8,24 +8,23 @@ yum install -y rpm-build yum-utils rpmdevtools gcc
 
 # Go to /tmp to get and extract collectd source base
 cd /tmp
-wget -c http://collectd.org/files/collectd-5.4.1.tar.bz2
+wget -c http://collectd.org/files/collectd-5.4.2.tar.bz2
 
 # Extract source
-tar xvf collectd-5.4.1.tar.bz2
+tar xvf collectd-5.4.2.tar.bz2
 
 # Copy sources in build directory
 mkdir -p /rpmbuild/SOURCES/
-cp collectd-5.4.1.tar.bz2 /rpmbuild/SOURCES/
+cp collectd-5.4.2.tar.bz2 /rpmbuild/SOURCES/
 
 # Fix the specfile to match collected version
-sed -i s/5.4.0/5.4.1/ collectd-5.4.1/contrib/redhat/collectd.spec
+sed -i s/5.4.0/5.4.2/ collectd-5.4.2/contrib/redhat/collectd.spec
 
 # Install dependencies of the spec file
-yum-builddep -y collectd-5.4.1/contrib/redhat/collectd.spec
+yum-builddep -y collectd-5.4.2/contrib/redhat/collectd.spec
 
 # Create RPMS
-rpmbuild -bb collectd-5.4.1/contrib/redhat/collectd.spec
+rpmbuild -bb collectd-5.4.2/contrib/redhat/collectd.spec
 
 # Copy RPMs back to the host
 cp -rv /rpmbuild/RPMS/ /data
-
